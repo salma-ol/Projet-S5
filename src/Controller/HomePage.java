0e5af422935d52ee0bce968e6611905f0d2bc34e
Antoine <ahaz1701@gmail.com>
2020-12-06 12:13:22 +0100

    mvc done

--- src/Controller/HomePage.java
+++ src/Controller/HomePage.java
@@ -24,6 +24,7 @@ import java.util.logging.Logger;
 public class HomePage implements ActionListener{
     
     private final GUIhomePage home;
+    
     public HomePage(GUIhomePage home){
         this.home = home;
     }
